03428ab8c6cfeb64b28ee4686617b6972dd19512
salma <drefsalma@gmail.com>
2025-12-03 13:29:37 +0100

    Feat:add Edit, show, cancel buttons

--- symfony/src/Controller/ItemWebController.php
+++ symfony/src/Controller/ItemWebController.php
@@ -16,20 +16,17 @@ class ItemWebController extends AbstractController
     public function new(Request $request, EntityManagerInterface $em): Response
     {
         $item = new Item();
-
-        // Set the currently logged-in user as owner
         $item->setOwner($this->getUser());
 
         $form = $this->createForm(ItemType::class, $item);
-
         $form->handleRequest($request);
+
         if ($form->isSubmitted() && $form->isValid()) {
             $item->setCreatedAt(new \DateTimeImmutable());
             $em->persist($item);
             $em->flush();
 
             $this->addFlash('success', 'Item added successfully!');
-           
             return $this->redirectToRoute('item_list');
         }
 
@@ -41,7 +38,7 @@ class ItemWebController extends AbstractController
     #[Route('/items/list', name: 'item_list')]
     public function list(EntityManagerInterface $em): Response
     {
-         $items = $em->getRepository(Item::class)->findBy([
+        $items = $em->getRepository(Item::class)->findBy([
             'owner' => $this->getUser()
         ]);
 
@@ -50,32 +47,74 @@ class ItemWebController extends AbstractController
         ]);
     }
 
-
     #[Route('/items/browse', name: 'item_browse')]
     public function browse(EntityManagerInterface $em): Response
     {
-          // Show all items regardless of owner
         $items = $em->getRepository(Item::class)->findBy([], ['createdAt' => 'DESC']);
 
         return $this->render('item/browse.html.twig', [
-             'items' => $items,
-    ]);
+            'items' => $items,
+        ]);
     }
-    
 
-    #[Route('/items/show/dummy', name: 'item_show_dummy')]
-    public function showDummy(): Response
+    #[Route('/items/{id}', name: 'item_show')]
+    public function show(int $id, EntityManagerInterface $em): Response
     {
-        $item = new Item();
-        $item->setTitle('Dummy Item Title');
-        $item->setDescription('This is an example description used while designing the template.');
-        $item->setStatus('Draft');
-        $item->setCreatedAt(new \DateTimeImmutable());
+        $item = $em->getRepository(Item::class)->find($id);
+        if (!$item) {
+            throw $this->createNotFoundException('Item not found.');
+        }
 
         return $this->render('item/show.html.twig', [
             'item' => $item,
         ]);
     }
 
+    #[Route('/items/{id}/edit', name: 'item_edit')]
+    public function edit(int $id, Request $request, EntityManagerInterface $em): Response
+    {
+        $item = $em->getRepository(Item::class)->find($id);
+        if (!$item) {
+            throw $this->createNotFoundException('Item not found.');
+        }
 
+        if ($item->getOwner() !== $this->getUser()) {
+            throw $this->createAccessDeniedException('You cannot edit this item.');
+        }
+
+        $form = $this->createForm(ItemType::class, $item);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->flush();
+            $this->addFlash('success', 'Item updated successfully');
+            return $this->redirectToRoute('item_show', ['id' => $item->getId()]);
+        }
+
+        return $this->render('item/edit.html.twig', [
+            'item' => $item,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/items/{id}/delete', name: 'item_delete', methods: ['POST'])]
+    public function delete(int $id, Request $request, EntityManagerInterface $em): Response
+    {
+        $item = $em->getRepository(Item::class)->find($id);
+        if (!$item) {
+            throw $this->createNotFoundException('Item not found.');
+        }
+
+        if ($item->getOwner() !== $this->getUser()) {
+            throw $this->createAccessDeniedException('You cannot delete this item.');
+        }
+
+        if ($this->isCsrfTokenValid('delete_item_' . $item->getId(), $request->request->get('_token'))) {
+            $em->remove($item);
+            $em->flush();
+            $this->addFlash('success', 'Item deleted successfully');
+        }
+
+        return $this->redirectToRoute('item_list');
+    }
 }
--- symfony/src/Form/ItemType.php
+++ symfony/src/Form/ItemType.php
@@ -4,6 +4,9 @@ namespace App\Form;
 
 use App\Entity\Item;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,9 +15,25 @@ class ItemType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-            ->add('description')
-            ->add('status')
+            ->add('title', TextType::class, [
+                'label' => 'Item Title',
+                'attr' => ['placeholder' => 'Enter item name'],
+            ])
+
+            ->add('description', TextareaType::class, [
+                'label' => 'Description',
+                'required' => false,
+                'attr' => ['placeholder' => 'Describe the item', 'rows' => 4],
+            ])
+
+            ->add('status', ChoiceType::class, [
+                'label' => 'Status',
+                'choices' => [
+                    'Lost' => 'lost',
+                    'Found' => 'found',
+                ],
+                'placeholder' => 'Select status',
+            ])
         ;
     }
 
